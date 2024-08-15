18f1b0bb349ac0b4ae5b3e83c45856ef3a429498
Desy Ayurianti <desii.ayurianti@gmail.com>
2024-08-16 06:27:25 +0700

    fix(sidebar): fix slicing sidebar

--- src/app/pages/sidebar/sidebar.component.ts
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthenticationService } from '../../core/authentication.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']); 
+  }
 }
